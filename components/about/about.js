bf16a2f4579ff76bcc5fae9712962fe4c5f6f4fb
jack hanford <jackhanford@gmail.com>
2015-03-26 23:06:23 -0700

    fixed carousel

--- components/about/about.js
+++ components/about/about.js
@@ -5,13 +5,10 @@ function AboutController() {
   console.log('About Component Loaded');
   // this.heading = 'About Component!';
 
-  $(document).ready(function(){
     $('.slideshow img:gt(0)').addClass('animated rollIn');
     setInterval(function(){
-      $('.slideshow :first-child').addClass('animated rollIn')
-      .next('img').addClass('rollIn')
+      $('.slideshow :first-child').addClass('animated rollIn').next('img').addClass('rollIn')
       .end().appendTo('.slideshow');},
       3000);
-  });
 
 }
