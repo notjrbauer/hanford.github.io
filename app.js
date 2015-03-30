dfbd6d18919af5018c0ff6feafbc8737fbcfb76b
jack hanford <jackhanford@gmail.com>
2015-03-29 17:10:58 -0700

    adding better animations

--- app.js
+++ app.js
@@ -13,4 +13,15 @@ function AppController($router) {
     { path: '/portfolio', component: 'portfolio' },
     { path: '/projects', component: 'projects' }
   ]);
+
+  function runFadeIn(elements) {
+    timeout = 300;
+    for (var i = elements.length - 1; i >= 0; i--) {
+      elements[i].className += " animated fadeInUp"
+    };
+  }
+
+  $(document).ready(function() {
+    runFadeIn(document.querySelectorAll('.animateMe'));
+  });
 }
